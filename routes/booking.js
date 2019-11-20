5fbb7cdef0c9af952d9151de3cc50686fde6e338
Kevin Jørgensen <kevinjoergensen@outlook.com>
2019-11-20 19:44:09 +0100

    småforbedringer i booking.pug

--- routes/booking.js
+++ routes/booking.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/booking', (req, res) => {
-    res.render('booking')
+    res.render('booking', {tid : tidspunkter})
 })
 
 module.exports = router;
+
+
+//Tidsvælger
+const tidspunkter = ['12-14', '14-16','16-18','18-20','20-22','22-00','00-02']
