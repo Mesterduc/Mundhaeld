85e27981dccc3deb5119ac484412a5f671700d60
Matias <eamrla@students.eaaa.dk>
2019-11-25 12:38:44 +0100

    jsdom

--- routes/booking.js
+++ routes/booking.js
@@ -1,24 +1,32 @@
 const express = require('express')
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const fs = require('fs')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom;
 
+const bookingpug = fs.readFileSync("./views/booking.pug")
+const document = new JSDOM(bookingpug);
+let window = document.window;
 
 router.get('/booking', (req, res) => {
-    res.render('booking', {tid : tidspunkter})
+  res.render('booking', {tid : tidspunkter, sluttid : sluttidspunkter})
+  //res.render('booking', {sluttid : sluttidspunkter})
 })
 
-
-router.post('', (req, res) => {
+router.post('/booking', (req, res) => {
   sendmail().catch().then(console.log(Error)),
   readInputs(),
-  res.render('booking', {tid: tidspunkter})
-  
+  res.render('booking', {tid: tidspunkter, sluttid: sluttidspunkter})
+  //res.render('booking', {sluttid: sluttidspunkter})
 })
+
 module.exports = router;
 
 
 //-------- Tidsvælger
 const tidspunkter = ['12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '00', '01', '02']
+let sluttidspunkter = ['12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '00', '01', '02'];
 let navn;
 let email;
 let telefon;
@@ -27,10 +35,22 @@ let beskrivelse;
 let dato;
 let starttidspunkt;
 let sluttidspunkt;
+//document.getElementsByName('starttidspunkt').selectedIndex
+
+starttid = window.document.getElementsByName('starttimepicker');
+window.addEventListener("change", ændreTid())
+function ændreTid() {
+  sluttidspunkter = [];
+  for (let i = 5; i < tidspunkter.length; i++) {
+    sluttidspunkter.push(tidspunkter[i]); 
+  }
+}
+
+//ændreTid()
 
 
 function readInputs(){
-  navn = document.getElementById('bName')
+  navn = window.document.getElementById('bName').innerHTML;
   email = document.getElementById('bEmail')
   telefon = document.getElementById('bPhone')
   antal = document.getElementById('bPersons')
