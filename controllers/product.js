e81a7cf2cbeb73aec29bb7ffa401b766413b8428
Duc hong cai <mesterduc@Ducs-MacBook-Pro.local>
2019-11-27 10:25:00 +0100

    slet og redigere i sortiment

--- controllers/product.js
+++ controllers/product.js
@@ -14,23 +14,11 @@ exports.createProduct = function(){
     return product.save()
 }
 
-exports.getProducts = function(){
-    return Product.find().exec((err, products)=>{
-        if(err) return handleError(err)
-        console.log(products)
-    })
-}
-
 exports.getProduct = function(productID){
     return Product.findOne({name: productID}).exec((err)=>{
         if(err) return handleError(err)
     })
 }
-exports.getCompanies = function() {
-    Product.find({name: "hej"}, function(err, arr) {
-        console.log("fuck")
-    });
-  };
 
 exports.getProdukt = async function() {
     return await Product.find({}).exec();
