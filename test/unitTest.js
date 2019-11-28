448f77a8132b72ebf4e7e92aaf00e21eca185dec
Sebastian Nissen <seb.nissen@live.dk>
2019-11-28 09:21:44 +0100

    Test er sat op

---
+++ test/unitTest.js
@@ -0,0 +1,13 @@
+const should = require('should')
+const mongoose = require('mongoose');
+const Produkt = require('../models/product');
+
+describe('Test suite', () => {
+
+    it('Find ID', () => {
+        
+        
+
+    })
+
+})
