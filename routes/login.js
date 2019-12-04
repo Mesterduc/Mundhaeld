405555494183c53aa6e97f11fd393a84a9f474b9
Kevin Jørgensen <kevinjoergensen@outlook.com>
2019-12-04 21:04:16 +0100

    Login virker og man skal logge ind før man kan gå til admin og adminsortiment

--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router();
 
 router.post('/login', (req, res) =>{
-let user=req.body.username
-let password=req.body.password
-if(user=="q"&&password=="123"){
-    res.redirect('admin')
-}else{
-    res.send("ingen adgang")
+if (req.body.username && req.body.username === 'user' && req.body.password && req.body.password === 'pass') {
+    req.session.authenticated = true;
+    res.redirect('/admin');
+} else {
+    console.log('fejl')
+    res.redirect('/login');
 }
+
 })
 
 router.get('/login', (req, res) => {
